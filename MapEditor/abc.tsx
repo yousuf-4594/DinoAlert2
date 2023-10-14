fb510aa7df0e982bf81f376f5c7c27b0e5816ecd
yousuf-4594 <cysiddiqui@gmail.com>
2023-10-14 15:21:47 +0500

    assets updated

---
+++ MapEditor/abc.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="abc" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <grid orientation="isometric" width="32" height="32"/>
+ <image source="../dinoalert/Assets/abc.png" width="256" height="256"/>
+</tileset>
